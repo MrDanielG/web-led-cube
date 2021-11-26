8792ae9f00580efb08c31f1b6924486505fd8998
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2021-11-26 17:56:00 -0600

    Add Initial Structure

--- src/App.js
+++ src/App.js
@@ -1,67 +1,93 @@
-import Paho from 'paho-mqtt';
+import { Card, Col, Container, Grid, Row, Text } from '@nextui-org/react';
+// import Paho from 'paho-mqtt';
 import './App.css';
-import logo from './logo.svg';
+import { default as patterns } from './data/patterns.json';
 
 function App() {
-    const clientId = 'client_id' + Math.floor(Math.random() * 1000000) + 1;
-    const client = new Paho.Client('driver.cloudmqtt.com', 38651, clientId);
+    // const clientId = 'client_id' + Math.floor(Math.random() * 1000000) + 1;
+    // const client = new Paho.Client('driver.cloudmqtt.com', 38651, clientId);
 
-    const doFail = (e) => console.log(e);
+    // const doFail = (e) => console.log(e);
 
-    const onConnect = () => {
-        console.log('Conexión exitosa');
-        client.subscribe('dgs_1');
-    };
+    // const onConnect = () => {
+    //     console.log('Conexión exitosa');
+    //     client.subscribe('dgs_1');
+    // };
 
-    const onMessageArrived = (message) => {
-        console.log('Un mensaje ha llegado: ' + message.payloadString);
-    };
+    // const onMessageArrived = (message) => {
+    //     console.log('Un mensaje ha llegado: ' + message.payloadString);
+    // };
 
-    const onConnectionLost = (responseObject) => {
-        if (responseObject.error !== 0) {
-            console.log('onConnectionLost: ', responseObject.errorMessage);
-        }
-    };
+    // const onConnectionLost = (responseObject) => {
+    //     if (responseObject.error !== 0) {
+    //         console.log('onConnectionLost: ', responseObject.errorMessage);
+    //     }
+    // };
 
-    const command = (value) => {
-        console.log(value);
-        const message = new Paho.Message(value + '');
-        message.destinationName = 'dgs_1';
-        client.send(message);
-    };
+    // const command = (value) => {
+    //     console.log(value);
+    //     const message = new Paho.Message(value + '');
+    //     message.destinationName = 'dgs_1';
+    //     client.send(message);
+    // };
 
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+    // client.onConnectionLost = onConnectionLost;
+    // client.onMessageArrived = onMessageArrived;
 
-    const options = {
-        useSSL: true,
-        userName: 'clxxwiek',
-        password: 'PtsiiKX9xhVm',
-        onSuccess: onConnect,
-        onFailure: doFail,
-    };
+    // const options = {
+    //     useSSL: true,
+    //     userName: 'clxxwiek',
+    //     password: 'PtsiiKX9xhVm',
+    //     onSuccess: onConnect,
+    //     onFailure: doFail,
+    // };
 
-    client.connect(options);
+    // client.connect(options);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <button onClick={() => command(0)}>0</button>
-                <button onClick={() => command(1)}>1</button>
-            </header>
-        </div>
+        <Container className="">
+            <Row justify="center" align="center">
+                <Text h1 color="black" style={{ margin: 50 }}>
+                    Proyecto Cubo Led
+                </Text>
+            </Row>
+            <Grid.Container gap={2} justify="center">
+                {patterns.map((pattern, i) => (
+                    <Grid xs={6} sm={4} key={i}>
+                        <Card width="100%" cover clickable hoverable>
+                            <Card.Header
+                                style={{
+                                    position: 'absolute',
+                                    zIndex: 1,
+                                    top: 5,
+                                }}
+                            >
+                                <Col>
+                                    <Text
+                                        size={12}
+                                        weight="bold"
+                                        transform="uppercase"
+                                        color="#ffffffAA"
+                                    >
+                                        {pattern.description}
+                                    </Text>
+                                    <Text h4 color="white">
+                                        {pattern.name}
+                                    </Text>
+                                </Col>
+                            </Card.Header>
+                            <Card.Image
+                                autoResize={false}
+                                src={pattern.img_url}
+                                height={200}
+                                width="100%"
+                                alt="Card image background"
+                            />
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid.Container>
+        </Container>
     );
 }
 
